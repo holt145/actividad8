19351f08f4cb88725a806ecc9f5550f05dd80015
holt145 <30914430+holt145@users.noreply.github.com>
2018-02-27 20:21:27 -0600

    Add files via upload

---
+++ act8/act8/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  act8
+//
+//  Created by Alumno IDS on 27/02/18.
+//  Copyright © 2018 Alumno IDS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
